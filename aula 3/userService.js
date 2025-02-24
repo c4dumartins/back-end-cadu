bb5b409080c11dd05dda35e748620a84693a661f
c4dumartins <carloslevado09@gmail.com>
2025-02-24 16:40:07 -0300

    aula2402

--- aula 3/userService.js
+++ aula 3/userService.js
@@ -6,9 +6,24 @@ const fs = require ('fs');
 class userService{
 
     constructor(){
+        this.filePath = path.join(__dirname, 'user.json');
         this.user = [];//Array para armazenar user
         this.nextId = 1;//contador para grrar id
     }
+
+    loadUsers(){
+        try { //tenta executar o bloco de codigo
+            if(fs.existsSync(this.filePath)){ //verifica se o arquivo existe
+                  const data = fs.readFileSync(this.filePath); //le o arquivo
+                  return JSON.parse(data); //transforma o json em objeto
+                }
+            } catch (erro) {
+                console.log('Erro ao carregar arquivo', erro);
+        }
+    }
+
+
+
     addUser(nome, email){
         const user = new User(this.nextId++, nome, email)
         this.user.push(user)
