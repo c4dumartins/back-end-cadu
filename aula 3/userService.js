662510f6de278e2f8ecf1dc08cb489b029397bdf
c4dumartins <carloslevado09@gmail.com>
2025-02-25 11:23:02 -0300

    aula 2502

--- aula 3/userService.js
+++ aula 3/userService.js
@@ -1,47 +1,65 @@
 const User = require("./user");
-const path = require ('path');
-const fs = require ('fs');
+const path = require('path');
+const fs = require('fs');
 
 
-class userService{
+class userService {
 
-    constructor(){
+    constructor() {
         this.filePath = path.join(__dirname, 'user.json');
-        this.user = [];//Array para armazenar user
-        this.nextId = 1;//contador para grrar id
+        this.users = this.loadUsers(); //Array para armazenar user
+        this.nextId = this.getNextId(); //contador para grrar id
     }
 
-    loadUsers(){
+    loadUsers() {
         try { //tenta executar o bloco de codigo
-            if(fs.existsSync(this.filePath)){ //verifica se o arquivo existe
-                  const data = fs.readFileSync(this.filePath); //le o arquivo
-                  return JSON.parse(data); //transforma o json em objeto
-                }
-            } catch (erro) { //caso ocorra um erro
-                console.log('Erro ao carregar arquivo', erro);
+            if (fs.existsSync(this.filePath)) { //verifica se o arquivo existe
+                const data = fs.readFileSync(this.filePath); //le o arquivo
+                return JSON.parse(data); //transforma o json em objeto
             }
-            return []; //retorna um array vazio
+        } catch (erro) { //caso ocorra um erro
+            console.log('Erro ao carregar arquivo', erro);
+        }
+        return []; //retorna um array vazio
+    }
+
+    //definir o próximo id a ser utilizado
+    getNextId() { //função para buscar o próximo id 
+        try {
+            if (this.users.length === 0) return 1;
+            return Math.max(...this.users.map(user => user.id)) + 1;
+        } catch (erro) {
+            console.log("Erro ao buscar proximo id", erro);
+        }
+    }
+
+    saveUsers() { //função para salvar os usuários
+        try {
+            fs.writeFileSync(this.filePath, JSON.stringify(this.users));
+        } catch (erro) {
+            console.log("Erro ao buscar proximo id", erro);
+        }
     }
 
 
-    getNextId(){
-        try{ 
-        if(this.users.length===0) return 1;
-        return Math.max(...this.users.map(user => user.id))+1; 
-    } catch (erro) {
-        console.log("Erro ao buscar proximo id", erro);
+    addUser(nome, email) {
+        try {
+            const user = new User(this.nextId++, nome, email);
+            this.users.push(user);
+            this.saveUsers();
+            return user;
+        } catch (erro) {
+            console.log("Erro ao buscar proximo id", erro);
+        }
+    }
+
+    getUsers() {
+        try {
+            return this.users;
+        } catch (erro) {
+            console.log("Erro ao buscar proximo id", erro);
+        }
     }
 }
 
-
-
-    addUser(nome, email){
-        const user = new User(this.nextId++, nome, email)
-        this.user.push(user)
-        return user;
-    }
-
-    getUsers(){
-        return this.user
-    }
-}
+module.exports = new userService
